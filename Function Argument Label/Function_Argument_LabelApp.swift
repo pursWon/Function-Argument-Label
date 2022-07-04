3b871a739c218ab4fdff793f757e268794ee05ac
Wongi <hpurs95@gmail.com>
2022-07-04 19:15:00 +0900

    Initial Commit

---
+++ Function Argument Label/Function_Argument_LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Function_Argument_LabelApp.swift
+//  Function Argument Label
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Function_Argument_LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
